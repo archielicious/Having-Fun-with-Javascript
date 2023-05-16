fd41b87ea08b3aa8f67ea3d39ee6f7577c95f583
archie <89709309+aholic-archie@users.noreply.github.com>
2023-05-16 14:29:24 +0530

    Update cyclicRotation.js
    Given an array of n integers and an integer k, rotate the array k times in clockwise order.

--- cyclicRotation.js
+++ cyclicRotation.js
@@ -13,10 +13,10 @@ function cyclicRotation(n, arr, k) {
         return arrAdd.concat(arr)
     }
    
-    // let ans=new Array(n);
-    // for (let i = n-1; i >=0; i--) {
-    //     let new_pos = (i + k)%n;
-    //     ans[new_pos] = arr[i];
-    // }
-    // return ans;
+//     let ans=new Array(n);
+//     for (let i = n-1; i >=0; i--) {
+//         let new_pos = (i + k)%n;
+//         ans[new_pos] = arr[i];
+//     }
+//     return ans;
 }
