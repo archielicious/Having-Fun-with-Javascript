5c265e5adcf3b12e9aa201192e8313a8ccead78b
archie <89709309+aholic-archie@users.noreply.github.com>
2023-05-16 14:28:03 +0530

    Update cyclicRotation.js

--- cyclicRotation.js
+++ cyclicRotation.js
@@ -12,4 +12,11 @@ function cyclicRotation(n, arr, k) {
         let arrAdd=arr.splice(n-k%n)
         return arrAdd.concat(arr)
     }
+   
+    // let ans=new Array(n);
+    // for (let i = n-1; i >=0; i--) {
+    //     let new_pos = (i + k)%n;
+    //     ans[new_pos] = arr[i];
+    // }
+    // return ans;
 }
