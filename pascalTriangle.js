7042440ed065b57424d5926ee40450f65a69249c
archie <89709309+aholic-archie@users.noreply.github.com>
2023-05-16 14:34:02 +0530

    Create pascalTriangle.js
    Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
    
    Pascal’s triangle is a pattern in which a number in a row b, is the sum of the number exactly above it a, and the number to the left of a, c.
    
    This makes the Pascal’s triangle all powers of 11 written row by row, except for the first row, which is 1.

---
+++ pascalTriangle.js
@@ -0,0 +1,14 @@
+function pascalTriangle(A) {
+  let R = [];
+  R.push([1]);
+  for (let i = 1; i < A; i++) {
+    let length = R[i - 1].length;
+    R.push([]);
+    R[i].push(1)
+    for (let j = 1; j < length; j++) {
+      R[i].push(R[i - 1][j] + R[i - 1][j - 1]);
+    }
+    R[i].push(1);
+  }
+  return R;
+}
