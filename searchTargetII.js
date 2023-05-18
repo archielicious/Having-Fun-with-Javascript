14f640d0d27553de706716cd2aebfe4690dd8b34
archie <89709309+aholic-archie@users.noreply.github.com>
2023-05-18 17:05:40 +0530

    Create searchTargetII.js
    Given an array of n elements and an integer x,find the index where x is present in the array. If there are multiple occurrences, find the rightmost one. If x is not present, return -1.

---
+++ searchTargetII.js
@@ -0,0 +1,22 @@
+function searchTargetII (N, arr, X){
+    let start = 0
+     let end = N - 1
+     let mid
+    let ans = -1;
+    while (start <= end) {
+        mid = Math.floor((start + end) / 2);
+        if (arr[mid] > X){
+            end = mid - 1;
+        }
+        else if (arr[mid] < X){
+            start = mid + 1;
+        }
+        else {
+            ans = mid;
+            start=mid+1            
+        }        
+    }
+    return ans;
+}
+
+// this function ( search ) returns the rightmost index of X inside the array N
