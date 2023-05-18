c57908bc035798f639a889485aa069e2ccf94246
archie <89709309+aholic-archie@users.noreply.github.com>
2023-05-18 15:10:41 +0530

    Update intersectionOfTwoArrays.js

--- intersectionOfTwoArrays.js
+++ intersectionOfTwoArrays.js
@@ -18,4 +18,26 @@ function intersectionOfTwoArrays(n, nums1, m, nums2){
         }
     }
     return inter
+    
+// let map=new Map()
+// // for (let i=0; i<n; i++){
+// for (i of nums1){
+//     // map.set(nums1[i], (map.get(nums1[i]) || 0)+1)
+//     map.set(i, (map.get(i) || 0)+1)
+// }
+// let result=[]
+// // for (let j=0; j<m; j++){
+// for (j of nums2){
+//     // if (map.has(nums2[j]) && map.get(nums2[j])>0){
+//     if (map.has(j) && map.get(j)>0){
+//         // result.push(nums2[j])
+//         result.push(j)
+//         // map.set(nums2[j], map.get(nums2[j])-1)
+//         map.set(j, map.get(j)-1)
+//     }
+// }
+// return result
+    
 }
+
+// in array, to traverse i of name[array] can be used, so instead of writing name[array][i] only i can be used
