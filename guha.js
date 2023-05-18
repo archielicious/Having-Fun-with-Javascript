835ae5553c2c38b5a777f6c1bdbd9e92ffd34797
archie <89709309+aholic-archie@users.noreply.github.com>
2023-05-18 23:16:55 +0530

    Update guha.js
    Given a name of the dish as a string print the string " kha" at the end of the dish using recursion.

--- guha.js
+++ guha.js
@@ -3,4 +3,11 @@ function guha(para){
         return " " + "kha"
     }
     return para[0]+guha(para.slice(1))
+    
+//     for (let i=0; i<para.length; i++){
+//         if (i===para.length-1){
+//             return `${para[i]} kha`
+//         }
+//         return para[i]+guha(para.slice(i+1))
+//     }
 }
