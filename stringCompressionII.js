77673bb6dd953d81f90d6aad7e414484a4d020d0
archie <89709309+aholic-archie@users.noreply.github.com>
2023-05-20 03:45:13 +0530

    Create stringCompressionII.js
    You are given a string S and you have to implement a function to perform basic string compression, using the counts of repeated characters.
    
    If the "compressed" string would not become smaller than the original string, your function should return the original string.
    
    You can assume the string has only uppercase and lowercase letters (a - z).

---
+++ stringCompressionII.js
@@ -0,0 +1,17 @@
+function stringCompression(s) {
+  let result=""
+  let count=1
+  for (let i=0; i<s.length; i++){
+    let current=s[i]
+    let next=s[i+1]
+    if (current===next){
+      count+=1
+    }
+    else{
+      result+=current+String(count)
+      count=1
+    }
+  }
+  return result.length<s.length ? result : s 
+
+}
