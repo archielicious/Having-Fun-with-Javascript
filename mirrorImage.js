15ff44b6344cfb26a5861d4631e1dd85dcb533dd
archie <89709309+aholic-archie@users.noreply.github.com>
2023-05-20 02:51:15 +0530

    Update mirrorImage.js
    You are given an n*m matrix, you have to find the mirror image of the matrix.

--- mirrorImage.js
+++ mirrorImage.js
@@ -1,7 +1,7 @@
 function mirrorImage(matrix){
     let row=matrix.length
     let col=matrix[0].length
-    if (col%2!==0){
+//     if (col%2!==0){
         for (let i=0; i<row; i++){
             for (j=0; j<Math.floor(col/2); j++){
                 let temp=matrix[i][col-j-1]
@@ -9,15 +9,15 @@ function mirrorImage(matrix){
                 matrix[i][j]=temp
             }
         }
-    }
-    else if (col%2==0){
-        for (let i=0; i<row; i++){
-            for (let j=0; j<col/2; j++){
-                let temp=matrix[i][col-j-1]
-                matrix[i][col-j-1]=matrix[i][j]
-                matrix[i][j]=temp
-            }
-        }
-    }
+//     }
+//     else if (col%2==0){
+//         for (let i=0; i<row; i++){
+//             for (let j=0; j<col/2; j++){
+//                 let temp=matrix[i][col-j-1]
+//                 matrix[i][col-j-1]=matrix[i][j]
+//                 matrix[i][j]=temp
+//             }
+//         }
+//     }
     return matrix
 }
