73a2ab1615054d46a2954d6f425335b71acaf3d6
archie <89709309+aholic-archie@users.noreply.github.com>
2023-05-20 03:24:08 +0530

    Create stringCompressionI.js
    You are given a string S and you have to implement a function to perform basic string compression, using the counts of repeated characters. Return a string containing single appearance of every character which is being repeated along with the number it is being repeated.
    
    If the "compressed" string would not become smaller than the original string, your function should return the original string.
    
    You can assume the string has only uppercase and lowercase letters (a - z).

---
+++ stringCompressionI.js
@@ -0,0 +1,39 @@
+    function stringCompressionI(s) {
+    // let strSet=new Set(s)
+    // let obj={}
+    // for (let i of strSet.values()){
+    //     obj[i]=0
+    // }
+    // let keyArr=Object.keys(obj)
+    // for (let i=0; i<keyArr.length; i++){
+    //     for (let j=0; j<s.length; j++){
+    //         if (s[j]===keyArr[i]){
+    //             obj[keyArr[i]]+=1
+    //         }
+    //     }
+    // }
+      
+    //  // for (i=0; i<s.length; i++){
+    //  //     if (!obj[s[i]]){
+    //  //         obj[s[i]]=1
+    //  //     }
+    //  //     else{
+    //  //         obj[s[i]]+=1
+    //  //     }
+    //  // }
+    // let str=""
+    // for (let k in obj){
+    //     str+=k+obj[k]
+    // }
+    // return str
+    
+    let map=new Map()
+    for (let i=0; i<s.length; i++){
+        map.set(s[i], (map.get(s[i]) || 0) + 1)
+    }
+    let str=""
+    for (let k of map.keys()){
+        str+=k+map.get(k)
+    }
+       return str
+}
