0537c520039a85a9c75089a1b02e1fcae6a28828
archie <89709309+aholic-archie@users.noreply.github.com>
2023-07-17 22:21:01 +0530

    Update counrPrimes.js
    Count the number of prime numbers upto a non-negative number, n.

--- counrPrimes.js
+++ counrPrimes.js
@@ -1,19 +1,38 @@
 function countPrimes(n) {
-    let count = 0
-    let primes=[]
-    for (let i = 2; i <= n; i++) {
-        let flag = "not found"
-        for (let j = 2; j <= parseInt(Math.sqrt(i)); j++) {
-            if (i % j === 0) {
-                flag = "found"
-                break
+    // let count = 0
+    // let primes=[]
+    // for (let i = 2; i <= n; i++) {
+    //     let flag = "not found"
+    //     for (let j = 2; j <= parseInt(Math.sqrt(i)); j++) {
+    //         if (i % j === 0) {
+    //             flag = "found"
+    //             break
+    //         }
+    //     }
+    //     if (flag === "not found") {
+    //         count += 1
+    //         primes.push(i)
+    //     }
+    // }
+    // console.log("The prime numbers are", primes)
+    // return count;
+
+    let prime=new Array(n+1)
+    for (let i=0; i<=n; i++){
+        prime[i]=1
+    }
+    for (let i=2; i*i<=n; i++){
+        if (prime[i]===1){
+            for (let j=i*i; j<=n; j=j+i){
+                prime[j]=0
             }
         }
-        if (flag === "not found") {
-            count += 1
-            primes.push(i)
+    }
+    let count=0
+    for (let i=2; i<=n; i++){
+        if (prime[i]===1){
+            count=count+1
         }
     }
-    console.log("The prime numbers are", primes)
-    return count;
+    return count
 }
