8b952026c2d3a8f029d3611889ade14f42989096
archie <89709309+aholic-archie@users.noreply.github.com>
2023-07-21 21:33:38 +0530

    Update validAnagram.js
    Given two strings s and t, write a function to determine if t is an anagram of s.
    
    Note: You may assume the string contains only lowercase alphabets.
    
    Note:-
    There's a test case where both the strings are empty but the js compiler is treating one as empty and other as undefined. So, update the readline function accordingly.

--- validAnagram.js
+++ validAnagram.js
@@ -1,30 +1,50 @@
 function validAnagram(s, t) {
-    let firstMap=new Map()
-    let secondMap=new Map()
+    // let firstMap=new Map()
+    // let secondMap=new Map()
+    // if (s.length==0 && t==undefined){
+    //     return true
+    // }
+    // else if (t==undefined){
+    //     return false
+    // }
+    // else{
+    // for (let i=0; i<s.length; i++){
+    //     firstMap.set(s[i], (firstMap.get(s[i]) || 0) + 1)
+    // }
+    // // console.log(firstMap)
+    // for (let j=0; j<t.length; j++){
+    //     secondMap.set(t[j], (secondMap.get(t[j]) || 0) + 1)
+    // }
+    // // console.log(secondMap)
+    // if (firstMap.size!==secondMap.size){
+    //     return false
+    // }
+    // for (let [key, value] of firstMap){
+    //     let testValue=secondMap.get(key)
+    //     if (testValue!==value || (testValue===undefined && secondMap.has(key))){
+    //         return false
+    //     }
+    // }
+    // return true
+    // }
+
     if (s.length==0 && t==undefined){
         return true
     }
     else if (t==undefined){
         return false
     }
-    else{
+    let frequency=new Array(26).fill(0)
     for (let i=0; i<s.length; i++){
-        firstMap.set(s[i], (firstMap.get(s[i]) || 0) + 1)
+        frequency[s[i].charCodeAt(0)-'a'.charCodeAt(0)]++
     }
-    // console.log(firstMap)
     for (let j=0; j<t.length; j++){
-        secondMap.set(t[j], (secondMap.get(t[j]) || 0) + 1)
+        frequency[t[j].charCodeAt(0)-'a'.charCodeAt(0)]--
     }
-    // console.log(secondMap)
-    if (firstMap.size!==secondMap.size){
-        return false
-    }
-    for (let [key, value] of firstMap){
-        let testValue=secondMap.get(key)
-        if (testValue!==value || (testValue===undefined && secondMap.has(key))){
+    for (let k=0; k<frequency.length; k++){
+        if (frequency[k]!==0){
             return false
         }
     }
     return true
-    }
 }
